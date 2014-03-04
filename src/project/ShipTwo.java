aeab27fb6da6090eb4b85fb109ffee60642c491a
mnmkami <mnmkami@gmail.com>
2014-03-04 21:11:57 +0800

    Update ShipTwo.java

--- src/project/ShipTwo.java
+++ src/project/ShipTwo.java
@@ -6,8 +6,9 @@ public class ShipTwo implements Drawable
   int hitBoxWidth;
   int shipLength;
   int shipWidth;
+  int health;
   
-  public ShipTwo (int xPos, int yPos, int hitBoxLength, int hitBoxWidth, int shipLength, int shipWidth)
+  public ShipTwo (int xPos, int yPos, int hitBoxLength, int hitBoxWidth, int shipLength, int shipWidth, int health)
   {
     this.xPos = xPos;
     this.yPos = yPos;
@@ -15,11 +16,43 @@ public class ShipTwo implements Drawable
     this.hitBoxWidth = hitBoxWidth;
     this.shipLength = shipLength;
     this.shipWidth = shipWidth;
+    this.health = health;
   }
   
-  public void Draw()
+  public void draw()
   {
     //insert how to draw ship here
+  }
+  
+  public void upMove()
+  {
+    yPos = yPos - 5;
+  }
+  
+  public void downMove()
+  {
+    yPos = yPos - 5;
+  }
+  
+  public void rightMove()
+  {
+    xPos = xPos + 5;
+  }
+  
+  public void leftMove()
+  {
+    xPos = xPos -5;
+  }
+  
+  public void shoot()
+  {
+    //probably creates a bullet at the current xPos and yPos
+  }
+  
+  public void takeDamage(int damage)
+  {
+    //possible to implement animation for getting hit?
+    health = health - damage;
   }
   
 }
